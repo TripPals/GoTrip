63dd3db4c1b4a399f1a6ec1fede360ceb4cd5bf9
roywangroytw <roywangroy.tw@gmail.com>
2021-12-21 15:46:55 +0800

    starting to work on spotfinder api controller logic & wrote a rake that can add a specific spot

--- app/controllers/api/v1/spotfinders_controller.rb
+++ app/controllers/api/v1/spotfinders_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::SpotfindersController < ApplicationController
 
   def search
-    respond_to do |format|
-        format.json { render :json => { :status => "OK", :content => "Hello you!" }, status => 200 }
+
+    # 把前端打這支api帶回來的input value抓起來
+    search_input = params[:query]
+    search_query = 
+    "
+      SELECT *
+      FROM spots
+      WHERE
+      spots.name like '%#{search_input}%'
+    "
+    @spots = ActiveRecord::Base.connection.execute(search_query)
+
+    if @spots 
+      # 把找到的資料丟回去給前端，用JSON的方式
+      
+    else
+      # 打我們自己的Service Object去跟google要資料
+      # google.call(search_input)
     end
+
   end
 
 end
