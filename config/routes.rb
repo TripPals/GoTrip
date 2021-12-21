d051249a93a8992188f935d32ea2e31c3b774f66
lihsin <lihsin0427@gmail.com>
2021-12-21 14:16:28 +0800

    <fix>SyntaxError & add sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  devise_for :users { omniauth_callbacks: "users/omniauth_callbacks"  }
- 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/auth/twitter/callback" => "authentications#github"
