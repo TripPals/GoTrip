44412db0d66952cbd91f15bf7a29237ac0598128
lihsin <lihsin0427@gmail.com>
2021-12-21 18:41:39 +0800

    <feat>add registrations controller for edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   
   validates_presence_of   :email, presence: true
   validates_uniqueness_of :email, { scope: :provider }
+  validates_presence_of   :password, { on: :create }
+  validates_length_of     :password, in: 6..128, if: lambda {self.password.present?}
+  validates_confirmation_of :password, if: lambda {self.password.present?}
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
