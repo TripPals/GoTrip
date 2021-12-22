a555cbfc32d990c9dd7112e72600849569b9f742
lihsin <lihsin0427@gmail.com>
2021-12-22 16:20:04 +0800

    edit layouts pages: messages and header

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/auth/:provider/callback" => "authentications#create"
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   # get "/auth/twitter/callback" => "authentications#github"
   # get "/auth/google/callback"  => "authentications#google"
   
