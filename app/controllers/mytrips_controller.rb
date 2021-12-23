5707a56f9b885d4b715ce65e0c362c346adaa2e4
roywangroytw <roywangroy.tw@gmail.com>
2021-12-23 11:24:46 +0800

    Fixed some issues related to this branch

--- app/controllers/mytrips_controller.rb
+++ app/controllers/mytrips_controller.rb
@@ -18,9 +18,10 @@ class MytripsController < ApplicationController
 
 #Add Schedule
   def create
-    @trip = Trip.new(trip_params.merge(end_date: trip_params[:start_date].to_date + trip_params[:length].to_i.days - 1.days))
 
-    
+    @trip = Trip.new(trip_params)
+    @trip.end_date = trip_params[:start_date].to_date + trip_params[:length].to_i.days - 1.days
+
     if @trip.save
       redirect_to "/mytrips"
     else
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,4 @@
 class Trip < ApplicationRecord
   validates :name, :length, :start_date, presence: true
+  validates :length, numericality: { greater_than: 0,  only_integer: true }
 end
