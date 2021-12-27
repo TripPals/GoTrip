8baf64091968f9f4ec2411ef46854bd8dc1e25b5
tsao <tsaoyuan@gmail.com>
2021-12-27 14:44:50 +0800

    init models_Schedule

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
   validates :name, :length, :start_date, presence: true
   validates :length, numericality: { greater_than: 0,  only_integer: true }
+  has_many :schedules
 end
