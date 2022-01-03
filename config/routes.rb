50abc699f4dd98aa5e722185373e969fce5d1890
lihsin <lihsin0427@gmail.com>
2022-01-04 00:25:44 +0800

    HTML&CSS: header1 before login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root "welcome#index"
+  get '/about' => 'welcome#about'
 
   devise_for :users, controllers: 
   { omniauth_callbacks: "users/omniauth_callbacks",
