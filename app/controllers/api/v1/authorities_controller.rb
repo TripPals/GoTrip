fd89e91d5a51f67d7e77ddb460dce8fe290fab84
Lilith <li6253462@gmail.com>
2022-01-07 12:47:43 +0800

    <feat> get all users in current_trip API

--- app/controllers/api/v1/authorities_controller.rb
+++ app/controllers/api/v1/authorities_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::AuthoritiesController < ApplicationController
 
     def alluser
-        @user_trip = UserTrip.find_by(trip_id: params[trip_id:])
+        # @users = UserTrip.find_by(trip_id: params[:trip_id]).user
+
+        # @trip = params[:trip_id]
+        # @users = UserTrip.joins(:users).where(@trip.present?)
+        trip_id = params[:trip_id]
+        @users = User.find_by_sql("SELECT user_trips.id, user_trips.role, users.name, users.email ,users.image FROM user_trips INNER JOIN users ON user_trips.user_id = users.id where trip_id = '#{trip_id}'")
+        # @trip = Trip.find(params[:trip_id])
+        # @users = @trip.user
+        
+        respond_to do |format|
+            format.json{render :json =>  @users , status => 200}
+        end
         
     end
 
