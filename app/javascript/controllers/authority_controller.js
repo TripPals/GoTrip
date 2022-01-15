dcd280587e76ca71cdcb572fa12fbaee977b5b78
Lilith <li6253462@gmail.com>
2022-01-15 19:08:53 +0800

    show each user pic

--- app/javascript/controllers/authority_controller.js
+++ app/javascript/controllers/authority_controller.js
@@ -39,9 +39,11 @@ export default class extends Controller {
 								rightBox.innerHTML = `
 								<div class="rightsection" data-controller="editrole">
 									<div class="rightinfo">
-									<div><img width="50px" height="50px" src="/images/fallback/default-avatar.jpg"></div>
-										<p>${name}</p>
-										<p>${email}</p>
+										<div class="user-pic"><img src="/images/fallback/default-avatar.jpg" /></div>
+										<div class="text">
+											<span>${name}</span>
+											<span>${email}</span>
+										</div>
 									</div>
 									<div class="rightdetail">
 										<div class="changerole">
@@ -68,9 +70,11 @@ export default class extends Controller {
 								rightBox.innerHTML = `
 									<div class="rightsection" data-controller="editrole">
 										<div class="rightinfo" >
-											<div><img width="50px" height="50px" " src= "${image}"></div>
-											<p>${name}</p>
-											<p>${email}</p>
+											<div class="user-pic"><img src= "${image}"></div>
+											<div class="text">
+												<span>${name}</span>
+												<span>${email}</span>
+											</div>
 										</div>
 										<div class="rightdetail">
 											<div class="changerole">
@@ -90,7 +94,7 @@ export default class extends Controller {
 									</div>
 								`
 								rightResultBox.appendChild(rightBox) 
-					}else{
+					}	else{
 						const rightResultBox = document.querySelector(".rightresultbox")
 						const rightBox = document.createElement("div")
 						rightBox.classList.add("rightresultdiv")
@@ -100,9 +104,11 @@ export default class extends Controller {
 						rightBox.innerHTML = `
 							<div class="rightsection" data-controller="editrole">
 								<div class="rightinfo" >
-									<div class="user-pic" ><img width="50px" height="50px" src="https://gogotrip.s3.amazonaws.com/uploads/user/avatar/${user_id}/${fileName}"></div>
-									<p>${name}</p>
-									<p>${email}</p
+									<div class="user-pic" ><img src="https://gogotrip.s3.amazonaws.com/uploads/user/avatar/${user_id}/${fileName}"></div>
+									<div class="text">
+										<span>${name}</span>
+										<span>${email}</span>
+									</div>
 								</div>
 								<div class="rightdetail">
 									<div class="changerole">
@@ -123,15 +129,19 @@ export default class extends Controller {
 						`
 						rightResultBox.appendChild(rightBox)
 					}
-				} else if (role === 2){
-								const rightResultBox = document.querySelector(".rightresultbox")
-								const rightBox = document.createElement("div")
-								rightBox.classList.add("rightresultdiv")
+			} else if (role === 2){
+						if (avatar.url === "/images/fallback/default-avatar.jpg"){
+							const rightResultBox = document.querySelector(".rightresultbox")
+							const rightBox = document.createElement("div")
+							rightBox.classList.add("rightresultdiv")
 								rightBox.innerHTML = `
 								<div class="rightsection" data-controller="editrole">
-									<div class="rightinfo" >
-										<p>${name}</p>
-										<p>${email}</p>
+									<div class="rightinfo">
+										<div class="user-pic"><img src="/images/fallback/default-avatar.jpg" /></div>
+										<div class="text">
+											<span>${name}</span>
+											<span>${email}</span>
+										</div>
 									</div>
 									<div class="rightdetail">
 										<div class="changerole">
@@ -139,36 +149,144 @@ export default class extends Controller {
 											<select name="role"id="role-select"  data-role="${role}" data-id="${ut_id}" 
 											data-editrole-target="selector" data-action="change->editrole#role" >
 												<option selected="selected" data-value="2">可編輯</option>
+												<option data-value="2">可檢視</option>
+											</select>
+											<i class="fas fa-check-circle hide-edit-role-checked" data-id="${ut_id}">已更新</i>
+										</div>
+										<div class="deleterole" data-editrole-target="deletebtn">
+											<button class="deletebtn" data-action="click->editrole#delete"><p>刪除成員</p>
+											</button>
+										</div>
+									</div>
+								</div>
+							`
+							rightResultBox.appendChild(rightBox)							
+							
+					} else if(image !== null){
+						const rightResultBox = document.querySelector(".rightresultbox")
+						const rightBox = document.createElement("div")
+						rightBox.classList.add("rightresultdiv")
+							rightBox.innerHTML = `
+								<div class="rightsection" data-controller="editrole">
+									<div class="rightinfo" >
+										<div class="user-pic"><img src= "${image}"></div>
+										<div class="text">
+											<span>${name}</span>
+											<span>${email}</span>
+										</div>
+									</div>
+									<div class="rightdetail">
+										<div class="changerole">
+											<label class="rightlabel" for="role-select">變更權限:</label>
+											<select name="role"id="role-select"  data-role="${role}" data-id="${ut_id}" 
+											data-editrole-target="selector" data-action="change->editrole#role" >
+												<option selected="selected" data-value="２">可編輯</option>
 												<option data-value="1">可檢視</option>
 											</select>
 											<i class="fas fa-check-circle hide-edit-role-checked" data-id="${ut_id}">已更新</i>
 										</div>
 										<div class="deleterole" data-editrole-target="deletebtn">
-											<button class="deletebtn" data-action="click->editrole#delete"><p>刪除成員</p></button>
+											<button class="deletebtn" data-action="click->editrole#delete"><p>刪除成員</p>
+											</button>
 										</div>
 									</div>
 								</div>
 							`
-							rightResultBox.appendChild(rightBox)
-							
-				} else{
+							rightResultBox.appendChild(rightBox) 
+					}else{
+						const rightResultBox = document.querySelector(".rightresultbox")
+						const rightBox = document.createElement("div")
+						rightBox.classList.add("rightresultdiv")
+						const avatarImg = avatar.url
+						const fileName = avatarImg.replace("https://gogotrip.s3.amazonaws.com/uploads/user/avatar/", "")
+						rightBox.innerHTML = `
+							<div class="rightsection" data-controller="editrole">
+								<div class="rightinfo" >
+									<div class="user-pic" ><img src="https://gogotrip.s3.amazonaws.com/uploads/user/avatar/${user_id}/${fileName}"></div>
+									<div class="text">
+										<span>${name}</span>
+										<span>${email}</span>
+									</div>
+								</div>
+								<div class="rightdetail">
+									<div class="changerole">
+										<label class="rightlabel" for="role-select">變更權限:</label>
+										<select name="role"id="role-select"  data-role="${role}" data-id="${ut_id}" 
+										data-editrole-target="selector" data-action="change->editrole#role" >
+											<option selected="selected" data-value="2">可編輯</option>
+											<option data-value="1">可檢視</option>
+										</select>
+										<i class="fas fa-check-circle hide-edit-role-checked" data-id="${ut_id}">已更新</i>
+									</div>
+									<div class="deleterole" data-editrole-target="deletebtn">
+											<button class="deletebtn" data-action="click->editrole#delete"><p>刪除成員</p>
+											</button>
+									</div>
+								</div>
+							</div>
+						`
+						rightResultBox.appendChild(rightBox)
+					}
+			}	else{
+					if (avatar.url === "/images/fallback/default-avatar.jpg"){
 					const rightResultBox = document.querySelector(".rightresultbox")
 					const rightBox = document.createElement("div")
 					rightBox.classList.add("main-rightresultdiv")
 							rightBox.innerHTML = `
 								<div class="main-rightsection" data-controller="editrole">
+									<p class="main-rightresultdiv-owner">行程擁有者</p>
 									<div class="main-rightinfo" >
-										<p class="main-rightresultdiv-owner">行程擁有者</p>
-										<p>${name}</p>
-										<p>${email}</p>
+										<div class="user-pic"><img src="/images/fallback/default-avatar.jpg" /></div>
+										<div class="text">
+											<p>${name}</p>
+											<p>${email}</p>
+										</div>
 									</div>
 								</div>
-								`
+							`
 							rightResultBox.insertAdjacentElement("afterbegin",rightBox)
 						
-						}
+				} else if(image!== null){
+						const rightResultBox = document.querySelector(".rightresultbox")
+						const rightBox = document.createElement("div")
+						rightBox.classList.add("main-rightresultdiv")
+								rightBox.innerHTML = `
+									<div class="main-rightsection" data-controller="editrole">
+										<p class="main-rightresultdiv-owner">行程擁有者</p>
+										<div class="main-rightinfo" >
+											<div class="user-pic"><img src="${image}" /></div>
+											<div class="text">
+												<p>${name}</p>
+												<p>${email}</p>
+											</div>
+										</div>
+									</div>
+								`
+								rightResultBox.insertAdjacentElement("afterbegin",rightBox)
+				} else{
+					const rightResultBox = document.querySelector(".rightresultbox")
+					const rightBox = document.createElement("div")
+					const avatarImg = avatar.url
+					const fileName = avatarImg.replace("https://gogotrip.s3.amazonaws.com/uploads/user/avatar/", "")
+					rightBox.classList.add("main-rightresultdiv")
+							rightBox.innerHTML = `
+								<div class="main-rightsection" data-controller="editrole">
+									<p class="main-rightresultdiv-owner">行程擁有者</p>
+									<div class="main-rightinfo" >
+										<div class="user-pic" ><img src="https://gogotrip.s3.amazonaws.com/uploads/user/avatar/${user_id}/${fileName}"></div>
+										<div class="text">
+											<p>${name}</p>
+											<p>${email}</p>
+										</div>
+									</div>
+								</div>
+							`
+							rightResultBox.insertAdjacentElement("afterbegin",rightBox)
+					}
+		
+				
 				}
-			)
+			})
 		}
 		renderData()
 	}
