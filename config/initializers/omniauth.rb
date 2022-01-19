19ac668936cfcbf598c44ab56ffe8eaa25efefaa
Lilith <li6253462@gmail.com>
2022-01-19 18:09:00 +0800

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
   {
     scope: "email, profile",
-    image_aspect_ratio: "square",
-    image_size: 50
+    # image_aspect_ratio: "square",
+    # image_size: 50
   }
 
 
