e90f1cd8d534366f0e472601cc225fbc347a7b86
Lilith <li6253462@gmail.com>
2022-01-20 17:11:35 +0800

    check third party

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,8 +16,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      
+      p 55555555555555
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      p session["devise.#{provider}_data"]
+      p 5555555555555555
+      p provider
       redirect_to new_user_registration_url
     end
   end
