398653ba1adb975465592e2e59b33b8f7fb227d7
lihsin <lihsin0427@gmail.com>
2022-01-21 21:32:04 +0800

    adjust valivalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   # validation
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {:scope => :provider}
   validates :password, presence: true, confirmation: true, length: { in: 6..128 }, on: :create
-  validates :password_confirmation, presence: true, if: -> {self.password.present?}
 
   has_many :user_trips, dependent: :delete_all
   has_many :trips, through: :user_trips
@@ -30,7 +29,6 @@ class User < ApplicationRecord
 
   def current_trip_role(trip_id)
     user_trips.find_by(trip_id: trip_id).role
-    # byebug
   end
 
   def show_image
